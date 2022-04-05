ab3c42eb55f61b499715f5074a7502f2815de914
Nikhil <nikhilpradhan363@gmail.com>
2022-04-05 19:46:05 +0530

    add/delete input fields

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,34 +13,29 @@ const useInput = initialValue => {
     ];
 };
 
-export default function Main(props) {
+export default function Main() {
 
     const [nameProps, resetName] = useInput("");
+
     const [profileProps, resetProfile] = useInput("");
     const [profile2Props, reset2Profile] = useInput("");
     const [profile3Props, reset3Profile] = useInput("");
     const [profile4Props, reset4Profile] = useInput("");
+
     const [educationProps, resetEducation] = useInput("");
     const [cgpaProps, resetCgpa] = useInput("");
     const [yearProps, resetYear] = useInput("");
-    const [education1Props, resetEducation1] = useInput("");
-    const [cgpa1Props, resetCgpa1] = useInput("");
-    const [year1Props, resetYear1] = useInput("");
-    const [education2Props, resetEducation2] = useInput("");
-    const [cgpa2Props, resetCgpa2] = useInput("");
-    const [year2Props, resetYear2] = useInput("");
+   
     const [skillProps, resetSkill] = useInput("");
-    const [skill1Props, resetSkill1] = useInput("");
-    const [skill2Props, resetSkill2] = useInput("");
-    const [skill3Props, resetSkill3] = useInput("");
-    const [skill4Props, resetSkill4] = useInput("");
-    const [skill5Props, resetSkill5] = useInput("");
+   
     const [projectProps, resetProject] = useInput("");
-    const [project2Props, reset2Project] = useInput("");
+   
     const [achievementProps, resetAchievement] = useInput("");
-    const [achievement2Props, reset2Achievement] = useInput("");
+   
     const [certificationsProps, resetCertifications] = useInput("");
+
     const [languagesProps, resetLanugages] = useInput("");
+
     const [interestProps, resetInterest] = useInput("");
 
 
@@ -48,31 +43,26 @@ export default function Main(props) {
         event.preventDefault();
 
         resetName();
+
         resetProfile();
         reset2Profile();
         reset3Profile();
         reset4Profile();
+
         resetEducation();
         resetCgpa();
         resetYear();
-        resetEducation1();
-        resetCgpa1();
-        resetYear1();
-        resetEducation2();
-        resetCgpa2();
-        resetYear2();
+       
         resetSkill();
-        resetSkill1();
-        resetSkill2();
-        resetSkill3();
-        resetSkill4();
-        resetSkill5();
+        
         resetProject();
-        reset2Project();
+       
         resetAchievement();
-        reset2Achievement();
+       
         resetCertifications();
+
         resetLanugages();
+
         resetInterest();
     };
     const [template, setTemplate] = useState(0);
@@ -90,45 +80,42 @@ export default function Main(props) {
     });
 
     return (
-        <div className='bg-[#eeeeee] flex flex-row'>
+        <div className='bg-[#eeeeee] flex flex-row h-full'>
 
             
-            <div className="container-build" align="center">
+            <div className='border-t-[#1e293b] border-r-[#1e293b] border-l-[#1e293b] bg-[#eeeeee] flex flex-row py-8' align="center">
                 
-                <button onClick = {handleClickLeft} className='flex items-center mr-8 px-8 py-2 my-auto border-4 border-[#1e293b] hover:bg-gray-600 hover:text-white'>
+                <button onClick = {handleClickLeft} className='flex items-center mr-8 px-8 py-2 my-auto border-4 border-[#1e293b]  hover:bg-gray-600 hover:text-white'>
                     <AiOutlineDoubleLeft size='30' />
                 </button>
                    
 
                 <Template
                     ref={componentRef}
+
                     name={nameProps}
+
                     profile={profileProps}
                     profile2={profile2Props}
                     profile3={profile3Props}
                     profile4={profile4Props}
+
                     education={educationProps}
                     cgpa={cgpaProps}
                     year={yearProps}
-                    education1={education1Props}
-                    cgpa1={cgpa1Props}
-                    year1={year1Props}
-                    education2={education2Props}
-                    cgpa2={cgpa2Props}
-                    year2={year2Props}
+                    
                     skills={skillProps}
-                    skills1={skill1Props}
-                    skills2={skill2Props}
-                    skills3={skill3Props}
-                    skills4={skill4Props}
-                    skills5={skill5Props}
+                   
                     projects={projectProps}
-                    projects2={project2Props}
+                   
                     achievements={achievementProps}
-                    achievements2={achievement2Props}
+                    
                     certifications={certificationsProps}
+
                     languages={languagesProps}
+
                     interest={interestProps}
+
                     template={template}
                     
                 />
--- src/components/template.js
+++ src/components/template.js
@@ -1,11 +1,52 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
+import React, { useState } from 'react';
+import { IoIosAddCircle } from "react-icons/io";
+import { AiFillDelete } from 'react-icons/ai';
+
 
 const Template = React.forwardRef((props, ref) => {
     // Changed className from 'card' to fake class 'tempcard'
-    // bg-[#FC4F4F] 
+
+    const [eduInputList, setEduInputList] = useState([
+        { index: Math.random(), edu: "", cgpa: "", year: "" },
+    ]);
+
+    const [skillInputList, setSkillInputList] = useState([
+        { index: Math.random(), skill: '' }
+    ]);
+
+    const handleEduChange = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...eduInputList];
+        list[index][name] = value;
+        setEduInputList(list);
+    }
+    const handleEduDeleteClick = (index) => {
+        const list = [...eduInputList];
+        list.splice(index, 1);
+        setEduInputList(list);
+    }
+    const handleEduAddClick = () => {
+        setEduInputList([...eduInputList, { index: Math.random(), edu: '', cgpa: '', year: '' }]);
+    }
+
+    const handleSkillChange = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...skillInputList];
+        list[index][name] = value;
+        setSkillInputList(list);
+    }
+    const handleSkillDeleteClick = (index) => {
+        const list = [...skillInputList];
+        list.splice(index, 1);
+        setSkillInputList(list);
+    }
+    const handleSkillAddClick = () => {
+        setSkillInputList([...skillInputList, { index: Math.random(), skill: '' }]);
+    }
+
+
     return (
-        <div ref={ref} className="border-4 border-slate-600 shadow-xl h-[1585px]">
+        <div ref={ref} className="border-4 border-slate-600 bg-[white] shadow-xl h-[1585px]">
 
             <div className={(props.template === 0) ? 'bg-[#506d84] text-white flex flex-col' : 'bg-[#668925] text-white flex flex-col'} >
                 {/* <div>
@@ -27,38 +68,73 @@ const Template = React.forwardRef((props, ref) => {
 
                 </div>
             </div>
-            <div className="bg-white py-4 px-8">
+            <div className=" py-4 px-8">
 
 
                 <h5 className='text-2xl flex flex-start font-bold font-serif border-b-2 border-black pt-4'>EDUCATION</h5>
 
-                <div className='flex flex-col text-xl pt-2 pb-4'>
-                    <div className='flex flex-row grid grid-cols-3'>
-                        <input type="text" placeholder="Edu1" {...props.education} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none " />
-                        <input type="text" placeholder='cgpa' {...props.cgpa} className='px-3 py-3 bg-white placeholder-gray-600 text-gray-900 border-0 outline-none' />
-                        <input type="text" placeholder='year' {...props.year} className='px-3 py-3 bg-white placeholder-gray-600 text-gray-900 border-0 outline-none' />
-                    </div>
-                    <div className='flex flex-row grid grid-cols-3'>
-                        <input type="text" placeholder="Edu2" {...props.education1} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none " />
-                        <input type="text" placeholder='cgpa/%' {...props.cgpa1} className='px-3 py-3 bg-white placeholder-gray-600 text-gray-900 border-0 outline-none' />
-                        <input type="text" placeholder='year' {...props.year1} className='px-3 py-3 bg-white placeholder-gray-600 text-gray-900 border-0 outline-none' />
-                    </div>
-                    <div className='flex flex-row grid grid-cols-3'>
-                        <input type="text" placeholder="Edu3" {...props.education2} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none " />
-                        <input type="text" placeholder='cgpa/%' {...props.cgpa2} className='px-3 py-3 bg-white placeholder-gray-600 text-gray-900 border-0 outline-none' />
-                        <input type="text" placeholder='year' {...props.yea2r} className='px-3 py-3 bg-white placeholder-gray-600 text-gray-900 border-0 outline-none' />
-                    </div>
+                {
+                    eduInputList.map((item, ind) => {
+                        return (
+                            <div className='flex flex-row text-xl pt-2 pb-4'>
+
+                                <div key={ind} className='flex flex-col grid grid-cols-3'>
+                                    <input type="text" placeholder="Edu" name='edu' value={eduInputList.edu} onChange={e => handleEduChange(e, ind)} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none " />
+                                    <input type="text" placeholder='cgpa' name='cgpa' value={eduInputList.cgpa} onChange={e => handleEduChange(e, ind)} className='px-3 py-3 bg-white placeholder-gray-600 text-gray-900 border-0 outline-none' />
+                                    <input type="text" placeholder='year' name='year' value={eduInputList.year} onChange={e => handleEduChange(e, ind)} className='px-3 py-3 bg-white placeholder-gray-600 text-gray-900 border-0 outline-none' />
+                                </div>
+
+                                {
+                                    eduInputList.length !== 1 &&
+                                    <div className='py-3 px-3'><AiFillDelete size='30' onClick={() => handleEduDeleteClick(ind)} /></div>
+                                }
+
+                                {
+                                    eduInputList.length - 1 === ind &&
+                                    <div className='py-3 px-3'><IoIosAddCircle size="30" onClick={handleEduAddClick} /></div>
+                                }
+
+                            </div>
+                        )
+
+                    })
+                }
+
+
+
+                {/* <pre>
+                    {JSON.stringify(eduInputList, null, 2)}
+                </pre> */}
+
+
+
+
+                <h5 className='text-2xl flex flex-start flex-row font-bold font-serif border-b-2 border-black pt-4'>SKILLS</h5>
+                <div className='flex flex-row grid grid-cols-2' >
+                    {
+                        skillInputList.map((item, ind) => {
+                            return (
+
+                                <div className='flex flex-row'>
+                                    <input key={ind} type="text" placeholder="skill" name="skill" value={skillInputList.skill} onChange={e => handleSkillChange(e, ind)} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
+                                    {
+                                        skillInputList.length !== 1 &&
+                                        <div className='py-3 px-3'><AiFillDelete size='30' onClick={() => handleSkillDeleteClick(ind)} /></div>
+                                    }
+
+                                    {
+                                        skillInputList.length - 1 === ind &&
+                                        <div className='py-3 px-3'><IoIosAddCircle size="30" onClick={handleSkillAddClick} /></div>
+                                    }
+                                </div>
+
+
+                            )
+                        })
+                    }
                 </div>
 
-                <h5 className='text-2xl flex flex-start font-bold font-serif border-b-2 border-black pt-4'>SKILLS</h5>
-                <div className='grid grid-cols-3'>
-                    <input type="text" placeholder="skill1" {...props.skills} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
-                    <input type="text" placeholder="skill2" {...props.skills1} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none text-xl " />
-                    <input type="text" placeholder="skill3" {...props.skills2} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none text-xl " />
-                    <input type="text" placeholder="skill4" {...props.skills3} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none text-xl " />
-                    <input type="text" placeholder="skill5" {...props.skills4} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none text-xl " />
-                    <input type="text" placeholder="skill6" {...props.skills5} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none text-xl " />
-                </div>
+
 
 
                 <h5 className='text-2xl flex flex-start font-bold font-serif border-b-2 border-black pt-4'>PROJECTS</h5>
@@ -68,29 +144,17 @@ const Template = React.forwardRef((props, ref) => {
                         <input type="text" placeholder="Project1" {...props.projects} className="px-2 py-2 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
                         <textarea type="text" placeholder="Desc1" {...props.desc} className="px-2 py-2 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
                     </div>
-                    <div className='flex flex-col'>
-                        <input type="text" placeholder="Project2" {...props.projects1} className="px-2 py-2 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
-                        <textarea type="text" placeholder="Desc2" {...props.desc1} className="px-2 py-2 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
-                    </div>
-                    <div className='flex flex-col'>
-                        <input type="text" placeholder="Project3" {...props.projects2} className="px-2 py-2 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
-                        <textarea type="text" placeholder="Desc3" {...props.desc2} className="px-2 py-2 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
-                    </div>
+
                 </div>
 
-               
+
 
                 <h5 className='text-2xl flex flex-start font-bold font-serif border-b-2 border-black pt-4'>ACHIEVEMENTS</h5>
                 <div className='flex flex-col items-start'>
                     <div>
                         <input type="text" placeholder="Achievement1" {...props.achievements} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
                     </div>
-                    <div>
-                        <input type="text" placeholder="Achievement2" {...props.achievements2} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
-                    </div>
-                    <div>
-                        <input type="text" placeholder="Achievement2" {...props.achievements2} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
-                    </div>
+
                 </div>
 
                 <h5 className='text-2xl flex flex-start font-bold font-serif border-b-2 border-black pt-4'>CERTIFICATIONS</h5>
@@ -98,26 +162,22 @@ const Template = React.forwardRef((props, ref) => {
                     <div>
                         <input type="text" placeholder="certification1" {...props.certifications} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
                     </div>
-                    <div>
-                        <input type="text" placeholder="certification2" {...props.certifications2} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
-                    </div>
+
                 </div>
-            
+
                 <h5 className='text-2xl flex flex-start font-bold font-serif border-b-2 border-black pt-4'>INTEREST</h5>
-                
+
                 <div className='flex flex-col items-start pt-4'>
                     <div>
                         <input type="text" placeholder="interest1" {...props.interest} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
                     </div>
-                    <div>
-                        <input type="text" placeholder="interest2" {...props.interest2} className="px-3 py-3 placeholder-gray-600 bg-white text-gray-900 border-0 outline-none  text-xl" />
-                    </div>
+
                 </div>
-             
 
-            </div>
 
-        </div>
+            </div >
+
+        </div >
     )
 });
 
